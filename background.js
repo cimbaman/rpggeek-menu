7eab45206698aa12b7768d07ae7ee971625c1b0b
cimbaman <nikola_cimba@outlook.com>
2019-09-25 08:01:46 +0200

    Added rolls and pop up

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({
-        characters: [{name:"Luka", banner:"[ImageID=4945609]\\n[OOC]HP 40/40, AC 18[/OOC]", color:"orange"}, {name:"Emet", banner:"[listitem=5874849]Emet[/listitem]\\n[OOC]HP 19+5/19, AC 13\\nSpell Slots: 1st 2/2[/OOC]", color:"Darkviolet"}]
+        rpgg_menu_characters: [{name:"Luka", banner:"[ImageID=4945609]\\n[OOC]HP 40/40, AC 18[/OOC]", color:"#FFA500"}, {name:"Emet", banner:"[listitem=5874849]Emet[/listitem]\\n[OOC]HP 19+5/19, AC 13\\nSpell Slots: 1st 2/2[/OOC]", color:"#9400D3"}]
     }, 
     function() {
       console.log("Character set");
